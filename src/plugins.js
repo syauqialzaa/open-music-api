63ddd4e11c2707d6176e08646281cf0a6495041b
Alza Syauqi <syauqialza7@gmail.com>
2024-01-04 19:42:15 +0700

    add authentications

--- src/plugins.js
+++ src/plugins.js
@@ -1,13 +1,29 @@
+// albums
 const albums = require('./api/albums')
-const songs = require('./api/songs')
 const AlbumsService = require('./services/postgres/albums-service')
-const SongsService = require('./services/postgres/songs-service')
 const AlbumsValidator = require('./validator/albums')
+
+// songs
+const songs = require('./api/songs')
+const SongsService = require('./services/postgres/songs-service')
 const SongsValidator = require('./validator/songs')
+
+// users
+const users = require('./api/users')
+const UsersService = require('./services/postgres/users-service')
+const UsersValidator = require('./validator/users')
+
+// authentications
+const authentications = require('./api/authentications')
+const AuthenticationsService = require('./services/postgres/authentications-service')
+const TokenManager = require('./token/token-manager')
+const AuthenticationsValidator = require('./validator/authentications')
 
 const ServerPlugins = async server => {
   const albumsService = new AlbumsService()
   const songsService = new SongsService()
+  const usersService = new UsersService()
+  const authenticationsService = new AuthenticationsService()
 
   await server.register([
     {
@@ -22,6 +38,22 @@ const ServerPlugins = async server => {
       options: {
         service: songsService,
         validator: SongsValidator
+      }
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator
+      }
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator
       }
     }
   ])
---
+++ src/services/postgres/users-service.js
@@ -0,0 +1,78 @@
+const { Pool } = require('pg')
+const InvariantError = require('../../exceptions/invariant-error')
+const { nanoid } = require('nanoid')
+const bcrypt = require('bcrypt')
+const NotFoundError = require('../../exceptions/not-found-error')
+const AuthenticationError = require('../../exceptions/authentication-error')
+
+class UsersService {
+  constructor () {
+    this._pool = new Pool()
+  }
+
+  async addUser ({ username, password, fullname }) {
+    await this.verifyNewUsername(username)
+
+    const id = `user-${nanoid(16)}`
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const query = {
+      text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
+      values: [id, username, hashedPassword, fullname]
+    }
+
+    const result = await this._pool.query(query)
+    if (!result.rows.length) {
+      throw new InvariantError('User failed to add.')
+    }
+
+    return result.rows[0].id
+  }
+
+  async verifyNewUsername (username) {
+    const query = {
+      text: 'SELECT username FROM users WHERE username = $1',
+      values: [username]
+    }
+
+    const result = await this._pool.query(query)
+    if (result.rows.length > 0) {
+      throw new InvariantError('Failed to add user. Username has been used.')
+    }
+  }
+
+  async getUserById (id) {
+    const query = {
+      text: 'SELECT * FROM users WHERE id = $1',
+      values: [id]
+    }
+
+    const result = await this._pool.query(query)
+    if (!result.rows.length) {
+      throw new NotFoundError('User not found.')
+    }
+
+    return result.rows[0]
+  }
+
+  async verifyUserCredential (username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username]
+    }
+
+    const result = await this._pool.query(query)
+    if (!result.rows.length) {
+      throw new AuthenticationError('The credentials you provided are incorrect.')
+    }
+
+    const { id, password: hashedPassword } = result.rows[0]
+    const match = await bcrypt.compare(password, hashedPassword)
+    if (!match) {
+      throw new AuthenticationError('The credentials you provided are incorrect.')
+    }
+
+    return id
+  }
+}
+
+module.exports = UsersService
