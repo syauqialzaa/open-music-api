fff7a35ea6330d32411aa0b2549b6ea6598a5a23
Alza Syauqi <syauqialza7@gmail.com>
2024-01-30 12:26:23 +0700

    revision submission: add constraint fk

--- migrations/1705744407667_create-table-user-album-likes.js
+++ migrations/1705744407667_create-table-user-album-likes.js
@@ -13,6 +13,9 @@ exports.up = pgm => {
       notNull: true
     }
   })
+
+  pgm.addConstraint('user_album_likes', 'fk_user_album_likes.user_id_users.id', 'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE')
+  pgm.addConstraint('user_album_likes', 'fk_user_album_likes.album_id_albums.id', 'FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE')
 }
 
 exports.down = pgm => {
